bd771c098b7f98e4e85b22445a28865cee9def4a
serdg0 <sergio.diazg92@gmail.com>
2019-12-10 13:41:25 -0600

    commas fixed(?)

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   performance: {
-    hints: false
+    hints: false,
   },
   module: {
     rules: [
@@ -15,21 +15,21 @@ module.exports = {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
           'file-loader',
-        ]
+        ],
       },
       {
         test: /\.css$/,
         use: [
           'style-loader', 
-          'css-loader'
-        ]
+          'css-loader',
+        ],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           'file-loader',
-        ]
-      }
-    ]
-  }
+        ],
+      },
+    ],
+  },
 };
