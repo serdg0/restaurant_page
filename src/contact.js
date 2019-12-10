ead02a34354704a90a8bbf43727d8977e098bf96
serdg0 <sergio.diazg92@gmail.com>
2019-12-10 13:13:29 -0600

    clean dist and start linting

--- src/contact.js
+++ src/contact.js
@@ -1,44 +1,44 @@
-import grid from "./grid";
+import grid from './grid';
 
 const contactPage = () => {
-    grid();
-    const body = document.getElementsByTagName('body')[0];
-    body.className = '';
-    body.classList.add('contact-bg');
-    const mainContent = document.getElementById('main-content');
-    const contactInfo = document.createElement("div");
-    contactInfo.classList.add('card', 'text-center');
-    const cardBody = document.createElement('div');
-    cardBody.classList.add('card-body');
-    const imageCredits = document.createElement('a');
-    imageCredits.classList.add('card-link');
-    imageCredits.setAttribute('href', "https://www.freepik.com/free-photos-vectors/frame");
-    imageCredits.innerHTML = 'Background photos created by valeria_aksakova - www.freepik.com';
-    const contactInfoChef = document.createTextNode("Sergio Diaz");
-    const chefDiv = document.createElement('h5');
-    chefDiv.classList.add('card-title');
-    chefDiv.appendChild(contactInfoChef);
-    const contactInfoPlace = document.createTextNode("Lake Buena Vista, FL 32830-0250");
-    const placeDiv = document.createElement('p');
-    placeDiv.classList.add('card-text');
-    placeDiv.appendChild(contactInfoPlace);
-    const contactInfoPhone = document.createTextNode("+1 (407) 827-7144");
-    const phoneDiv = document.createElement('p');
-    phoneDiv.classList.add('card-text');
-    phoneDiv.appendChild(contactInfoPhone);
-    const contactInfoEmail = document.createTextNode("tacorest@tacorest.com.mx");
-    const emailDiv = document.createElement('p');
-    emailDiv.classList.add('card-text');
-    emailDiv.appendChild(contactInfoEmail);
-    let arr = [chefDiv, placeDiv, phoneDiv, emailDiv, imageCredits];
-    
-    arr.forEach(info => {
-        cardBody.appendChild(info);
-    });
-    contactInfo.appendChild(cardBody);
-    mainContent.innerHTML = '';
-    mainContent.appendChild(contactInfo);
-    return content;
-}
+  grid();
+  const body = document.getElementsByTagName('body')[0];
+  body.className = '';
+  body.classList.add('contact-bg');
+  const mainContent = document.getElementById('main-content');
+  const contactInfo = document.createElement('div');
+  contactInfo.classList.add('card', 'text-center');
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  const imageCredits = document.createElement('a');
+  imageCredits.classList.add('card-link');
+  imageCredits.setAttribute('href', 'https://www.freepik.com/free-photos-vectors/frame');
+  imageCredits.innerHTML = 'Background photos created by valeria_aksakova - www.freepik.com';
+  const contactInfoChef = document.createTextNode('Sergio Diaz');
+  const chefDiv = document.createElement('h5');
+  chefDiv.classList.add('card-title');
+  chefDiv.appendChild(contactInfoChef);
+  const contactInfoPlace = document.createTextNode('Lake Buena Vista, FL 32830-0250');
+  const placeDiv = document.createElement('p');
+  placeDiv.classList.add('card-text');
+  placeDiv.appendChild(contactInfoPlace);
+  const contactInfoPhone = document.createTextNode('+1 (407) 827-7144');
+  const phoneDiv = document.createElement('p');
+  phoneDiv.classList.add('card-text');
+  phoneDiv.appendChild(contactInfoPhone);
+  const contactInfoEmail = document.createTextNode('tacorest@tacorest.com.mx');
+  const emailDiv = document.createElement('p');
+  emailDiv.classList.add('card-text');
+  emailDiv.appendChild(contactInfoEmail);
+  const arr = [chefDiv, placeDiv, phoneDiv, emailDiv, imageCredits];
+
+  arr.forEach((info) => {
+    cardBody.appendChild(info);
+  });
+  contactInfo.appendChild(cardBody);
+  mainContent.innerHTML = '';
+  mainContent.appendChild(contactInfo);
+  return content;
+};
 
 export default contactPage;
--- src/home.js
+++ src/home.js
@@ -1,19 +1,16 @@
-import grid from "./grid";
-
 const home = () => {
-    const body = document.getElementsByTagName('body')[0];
-    body.className = '';
-    body.classList.add('body-center');
-    body.classList.add('home-bg');
-    const content = document.getElementById("content");
-    const divWelcome = document.createElement('div');
-    divWelcome.classList.add('text-center',  'h-100', 'align-items-center', 'mt-5');
-    const message = document.createElement('h1');
-    message.classList.add('welcome-msg');
-    message.innerHTML = 'Welcome to the new dimension of taste!';
-    divWelcome.appendChild(message);
-    content.innerHTML = '';
-    content.appendChild(divWelcome);
-}
+  const body = document.getElementsByTagName('body')[0];
+  body.className = '';
+  body.classList.add('body-center', 'home-bg');
+  const content = document.getElementById('content');
+  const divWelcome = document.createElement('div');
+  divWelcome.classList.add('text-center', 'h-100', 'align-items-center', 'mt-5');
+  const message = document.createElement('h1');
+  message.classList.add('welcome-msg');
+  message.innerHTML = 'Welcome to the new dimension of taste!';
+  divWelcome.appendChild(message);
+  content.innerHTML = '';
+  content.appendChild(divWelcome);
+};
 
 export default home;
